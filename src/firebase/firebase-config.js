3ca7c4341e308884cd53bf11e04338b61b1b2f71
Liav <oneandonlyliav@gmail.com>
2022-05-26 14:43:16 -0400

    add firebase dependencies

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyAe3WUJ9sSXaJuSoeVhejz-2jG04ldJxbI',
+  authDomain: 'message-board-f9ffa.firebaseapp.com',
+  projectId: 'message-board-f9ffa',
+  storageBucket: 'message-board-f9ffa.appspot.com',
+  messagingSenderId: '850772754423',
+  appId: '1:850772754423:web:fbf2db088f327b6c9778f2',
+};
