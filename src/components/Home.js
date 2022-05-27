bd0d1be2648118f640ec4bff53e2308e15b842a4
Liav <oneandonlyliav@gmail.com>
2022-05-26 22:37:48 -0400

    split out Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Topics from './Topics';
 
 const StyledHome = styled.div`
   display: flex;
@@ -6,42 +7,10 @@ const StyledHome = styled.div`
   border: 1px solid white;
 `;
 
-const ServersContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background: #222222;
-  border: 1px solid white;
-  width: 15rem;
-`;
-
-const ServersHeader = styled.h1`
-  border: 1px solid white;
-  color: white;
-  text-align: center;
-`;
-
-const ServerList = styled.div`
-  border: 1px solid white;
-  height: 100%;
-`;
-
-const ConversationsContainer = styled(ServersContainer)``;
-
-const ConversationsHeader = styled(ServersHeader)``;
-
-const ConversationList = styled(ServerList)``;
-
 const Home = () => {
   return (
     <StyledHome>
-      <ServersContainer>
-        <ServersHeader>Servers</ServersHeader>
-        <ServerList />
-      </ServersContainer>
-      <ConversationsContainer>
-        <ConversationsHeader>Conversations</ConversationsHeader>
-        <ConversationList />
-      </ConversationsContainer>
+      <Topics />
     </StyledHome>
   );
 };
