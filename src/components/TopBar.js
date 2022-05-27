c27bbe963071fce939389ab3cb92f2422b50aa93
Liav <oneandonlyliav@gmail.com>
2022-05-26 22:37:35 -0400

    split out TopBar component

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,57 +1,36 @@
 import styled from 'styled-components';
+import UserPanel from './UserPanel';
 
 const StyledTopBar = styled.div`
   display: flex;
   width: 100%;
   height: 4rem;
   border: 1px solid white;
-`;
-
-const StyledUserMenu = styled.div`
-  display: flex;
-  width: 15rem;
-  margin-left: auto;
-  background: #333333;
-  border: 1px solid white;
-`;
-
-const StyledProfilePictureContainer = styled.div`
-  width: 4rem;
-  height: 100%;
-  background: #666666;
-  border-radius: 50%;
-  border: 1px solid white;
-`;
-
-const StyledClickMenu = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 10rem;
-  margin-left: auto;
-  background: #666666;
-  border: 1px solid white;
-  color: white;
+  background: #444444;
 `;
 
 const HomeBar = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 15rem;
   height: 100%;
   border: 1px solid white;
   background: #222222;
 `;
 
-const ContextBar = styled(HomeBar)``;
+const HomeText = styled.p`
+  color: white;
+  font-size: 2rem;
+`;
 
 const TopBar = () => {
   return (
     <StyledTopBar>
-      <HomeBar />
-      <ContextBar />
-      <StyledUserMenu>
-        <StyledProfilePictureContainer />
-        <StyledClickMenu>Menu</StyledClickMenu>
-      </StyledUserMenu>
+      <HomeBar>
+        <HomeText>nublet</HomeText>
+      </HomeBar>
+      <UserPanel />
     </StyledTopBar>
   );
 };
