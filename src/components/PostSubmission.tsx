7cdca599f479560fc4143be7d854c90c45a417e8
Liav <oneandonlyliav@gmail.com>
2022-11-09 16:51:18 -0500

    fix for avatar placement within PostSubmission

--- src/components/PostSubmission.tsx
+++ src/components/PostSubmission.tsx
@@ -5,6 +5,7 @@ import { UserType, PostType } from '../types/PropTypes';
 import { Div } from '../styles/Div';
 import { Button } from '../styles/Button';
 import { TextArea } from '../styles/TextArea';
+import { Link } from 'react-router-dom';
 
 const StyledPostSubmission = styled(Div)`
   display: flex;
@@ -12,16 +13,34 @@ const StyledPostSubmission = styled(Div)`
   gap: 0.5rem;
 `;
 
+const AvatarLink = styled(Link)`
+  max-width: 3rem;
+  max-height: 3rem;
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 3;
+`;
+
+const Avatar = styled.img`
+  display: block;
+  max-width: 3rem;
+  max-height: 3rem;
+  border-radius: 100%;
+`;
+
 const PostSubmissionForm = styled.form`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
+  gap: 0.5rem;
 `;
 
 const BodyField = styled(TextArea)`
   min-height: 100px;
   width: 100%;
   height: 100%;
+  padding: 0.5rem;
 `;
 
 const SubmitButton = styled(Button)`
@@ -41,6 +60,9 @@ type PostSubmissionProps = InferProps<typeof PostSubmissionPropTypes>;
 const PostSubmission = ({ user, post }: PostSubmissionProps) => {
   return (
     <StyledPostSubmission>
+      <AvatarLink to={`/${user.handle}`}>
+        <Avatar src={user.avatar} />
+      </AvatarLink>
       <PostSubmissionForm onSubmit={(e) => submitPost(e, { user, post })}>
         <Div>
           <BodyField name="body" id="body" placeholder="..."></BodyField>
