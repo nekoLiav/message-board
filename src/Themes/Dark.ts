32687ca0d5a91b49f81941b2b8697638cafcee5f
Liav <oneandonlyliav@gmail.com>
2022-11-09 01:02:51 -0500

    adjust theme color schemes

--- src/Themes/Dark.ts
+++ src/Themes/Dark.ts
@@ -1,13 +1,26 @@
+const Neutral = {
+  50: '#fafafa',
+  100: '#f5f5f5',
+  200: '#e5e5e5',
+  300: '#d4d4d4',
+  400: '#a3a3a3',
+  500: '#737373',
+  600: '#525252',
+  700: '#404040',
+  800: '#262626',
+  900: '#171717',
+};
+
 export default {
-  fg: '#f1f5f9',
-  fg2: '#cbd5e1',
-  bg: '#0f172a',
-  brd: '#94a3b8',
+  fg: Neutral[100],
+  fg2: Neutral[300],
+  bg: Neutral[900],
+  brd: Neutral[400],
   btnbg: `linear-gradient(
     90deg,
     rgba(0, 255, 255, 1) 0%,
     rgba(255, 0, 255, 1) 100%
   )`,
-  main: '#1e293b',
-  mainmo: '#334155',
+  main: Neutral[800],
+  mainmo: Neutral[700],
 };
--- src/Themes/Light.ts
+++ src/Themes/Light.ts
@@ -1,13 +1,26 @@
+const Neutral = {
+  50: '#fafafa',
+  100: '#f5f5f5',
+  200: '#e5e5e5',
+  300: '#d4d4d4',
+  400: '#a3a3a3',
+  500: '#737373',
+  600: '#525252',
+  700: '#404040',
+  800: '#262626',
+  900: '#171717',
+};
+
 export default {
-  fg: '#0f172a',
-  fg2: '#475569',
-  bg: '#f1f5f9',
-  brd: '#64748b',
+  fg: Neutral[900],
+  fg2: Neutral[700],
+  bg: Neutral[100],
+  brd: Neutral[600],
   btnbg: `linear-gradient(
     90deg,
     rgba(0, 255, 255, 1) 0%,
     rgba(255, 0, 255, 1) 100%
   )`,
-  main: '#e2e8f0',
-  mainmo: '#cbd5e1',
+  main: Neutral[200],
+  mainmo: Neutral[300],
 };
