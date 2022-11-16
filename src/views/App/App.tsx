afc5291548808dd6c17cc373dafee654ab12364d
Liav <oneandonlyliav@gmail.com>
2022-11-16 06:49:55 -0500

    move global css to assets, move themeprovider

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,7 +1,4 @@
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from '../..';
 import { Outlet } from 'react-router-dom';
-import dark from '../../themes/Dark';
 import Header from '../../components/Header/Header';
 import Aside from '../../components/Sidebar/Sidebar';
 import { AppContainer, RouteContainer } from './style';
@@ -10,18 +7,15 @@ import { Suspense } from 'react';
 
 const App = () => {
   return (
-    <ThemeProvider theme={dark}>
-      <GlobalStyle />
-      <AppContainer>
-        <Header />
-        <RouteContainer>
-          <Suspense fallback={<Loading />}>
-            <Outlet />
-          </Suspense>
-        </RouteContainer>
-        <Aside />
-      </AppContainer>
-    </ThemeProvider>
+    <AppContainer>
+      <Header />
+      <RouteContainer>
+        <Suspense fallback={<Loading />}>
+          <Outlet />
+        </Suspense>
+      </RouteContainer>
+      <Aside />
+    </AppContainer>
   );
 };
 
