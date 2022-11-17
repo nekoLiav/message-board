385ad63b630607ef3e1e7fc7f2eb1c2589c4b1d1
Liav <oneandonlyliav@gmail.com>
2022-11-17 11:27:41 -0500

    remove local firebase db data due to directory change

--- src/tests/firebase-emulator/functions/index.js
+++
@@ -1,7 +0,0 @@
-// const functions = require('firebase-functions');
-
-// exports.myFunction = functions.firestore
-//   .document('users/{user}')
-//   .onCreate((snap) => {
-//     return snap.ref.update({ submissions: [], replies: [] });
-//   });
