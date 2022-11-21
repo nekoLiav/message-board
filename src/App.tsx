1367813c3de9dbe480abc9248419872754b0221f
Liav <oneandonlyliav@gmail.com>
2022-11-21 12:56:26 -0500

    remove react query from project

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from 'providers/QueryProvider';
 import { Loading } from 'pages/Loading';
 import { MainLayout } from 'components/Layouts';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <MainLayout>
-        <Suspense fallback={<Loading />}>
-          <Outlet />
-        </Suspense>
-      </MainLayout>
-      <ReactQueryDevtools initialIsOpen={false} position={'bottom-right'} />
-    </QueryClientProvider>
+    <MainLayout>
+      <Suspense fallback={<Loading />}>
+        <Outlet />
+      </Suspense>
+    </MainLayout>
   );
 };
 
